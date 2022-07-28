9b2791858c33bfed2e9e1a2ada439d9ff6746b70
Donovan Crader <craderdonovan@gmail.com>
2022-07-27 21:01:07 -0700

    done for today
    done for today

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// high-level
+//    do not have to manage resources because these programs have     extractions that take away these reposnibilites. will never be as fast as languages where you specify memory use but much easier
+
+// garbage-collected
+//    javascript automatically removes old, unused objects fromt he computer memory in order not to clog it up with unneccessary stuff
+
+// interpreted or just-in-time compiled
+//    our code is translated into 0s and 1s in order to be turned into machine code that the computer can read
+
+// multi-paradigm
+//    a paradigm is a n approach and overall mindeset about how we structure our code, which ulitmately directs the coding style and technique - examples: procedural, object-oriented, and functional programming. imperative or decla rative
+//    javascript is all of it: procedural, object-oriented, and functional
+
+// prototype-Based
+//
+
+
+// dynamic
+//
+
+// single-threaded
+//
+
+
+// first-calss functions
+//
+
+// event loop concurrency model
+//
